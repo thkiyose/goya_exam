3c58ed9d7769ddccd7afea5910cb95d8e4100bae
Haruka Teduka <th_kiyose@outlook.com>
2020-05-13 18:58:59 +0900

    test.rbを追加

---
+++ test.rb
@@ -0,0 +1,15 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler,'goya.rb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler,'exam.html.erb')
+server.mount('/goya_info.cgi', WEBrick::HTTPServlet::CGIHandler,'goya_info.rb')
+server.start
